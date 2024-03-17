cff1a514397ce8b45ed4e5e7a7e573ec92f91f08
Simon Lund <simon97lund@gmail.com>
2024-03-17 23:33:45 +0100

    :lipstick: on scoll hide hint with fade animation

--- src/components/ScrollHint/indext.tsx
+++ src/components/ScrollHint/indext.tsx
@@ -1,12 +1,26 @@
 import {BsMouse} from "react-icons/bs";
 import * as React from "react";
+import clsx from "clsx";
 
 const ScrollHint = () => {
+    const [showScrollHint, setShowScrollHint] = React.useState(false);
+
+    React.useEffect(() => {
+        const handleScroll = () => {
+            setShowScrollHint(window.scrollY < 10);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className="flex h-24 justify-center items-center"
              style={{background: "white", boxShadow: "0 0 5px 2px white"}}
         >
-            <BsMouse size={24} className={"animate-bounce"}/>
+            <BsMouse size={24}
+                className={clsx("animate-bounce transition-opacity duration-1000", showScrollHint ? "opacity-100" : "opacity-0")}
+            />
         </div>
     )
 }
