d4831f3ac1a7ade14f71c435277d67d1e58e7961
Simon Lund <simon97lund@gmail.com>
2024-03-17 23:08:56 +0100

    :fire: remove old styling & todo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,13 @@ import Hero from "../components/Hero";
 import {BsMouse} from "react-icons/bs";
 import About from "../components/About";
 import Projects from "../components/Projects";
+import Footer from "../components/Footer";
 import ScrollHint from "../components/ScrollHint/indext";
 
 const IndexPage: React.FC<PageProps> = () => {
     return (
-        <main className="relative">
+        <main>
             <div className="flex flex-col h-svh">
-                {/*TODO: now make sure that the header + hero + mouse indicator = full screen on all devices*/}
                 <Header/>
                 <Hero/>
                 <ScrollHint/>
