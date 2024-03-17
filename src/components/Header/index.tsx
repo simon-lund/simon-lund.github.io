6d46de5d12f6764db7de7618925476ff48c62968
Simon Lund <simon97lund@gmail.com>
2024-03-17 23:01:33 +0100

    :sparkles: add link to github page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import {Link} from "gatsby";
+import GithubMarkWhite from "../../images/github-mark-white.png";
 
 const Header = () => {
     return (
         <header className="fixed z-[100] w-screen h-12 backdrop-blur-md bg-black/90">
-            <div className="flex h-full items-center mx-8">
+            <div className="flex h-full justify-between items-center mx-8">
                 <Link
                     to="/"
                     className="text-white text-xl font-bold hover:cursor-pointer hover:rotate-12 transition-transform"
@@ -12,9 +13,14 @@ const Header = () => {
                 >
                     $&
                 </Link>
+
+                <a href="https://github.com/simon-lund">
+                    <img src={GithubMarkWhite as string} alt="Github" className="h-8 w-8"/>
+                </a>
             </div>
         </header>
-    );
+    )
+        ;
 }
 
 export default Header;
