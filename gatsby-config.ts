e7b31742a1e1a393109f9cc62ca6672154c2ad9b
Simon Lund <simon97lund@gmail.com>
2024-03-17 17:32:23 +0100

    :wrench: add gatsby-plugin-react-svg plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,22 +1,33 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Simon Lund `,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    siteMetadata: {
+        title: `Simon Lund `,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "pages"
-  }]
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        "gatsby-plugin-postcss", "gatsby-transformer-remark",
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        },
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /assets/ // See below to configure properly
+                }
+            }
+        }
+    ]
 };
 
 export default config;
